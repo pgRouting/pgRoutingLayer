8268f1dcf0a53672f0e1a51a0c37b073795bca36
cvvergara <cvvergara@georepublic.de>
2017-08-10 22:31:07 -0500

    removing the annotations from the project file

--- pgRoutingLayer.py
+++ pgRoutingLayer.py
@@ -869,8 +869,10 @@ class PgRoutingLayer:
         for anno in self.canvasItemList['annotations']:
             try:
                 anno.setVisible(False)
+                self.iface.mapCanvas().scene().removeItem(anno)
             except RuntimeError, e:
-                Utils.logMessage("anno.setVisible(False) failed, " + e.message, QgsMessageLog.WARNING)
+                QApplication.restoreOverrideCursor()
+                QMessageBox.critical(self.dock, self.dock.windowTitle(), '%s' % e)
         self.canvasItemList['annotations'] = []
         for path in self.canvasItemList['paths']:
             path.reset(Utils.getRubberBandType(False))
