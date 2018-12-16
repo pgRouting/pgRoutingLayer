55e1cf208e27bbf26d68fbed54cc7b50b226a2a1
cvvergara <vicky@georepublic.de>
2018-12-16 08:21:52 -0600

    pgr_bdAstar to work like pgr_aStar

--- functions/bdAstar.py
+++ functions/bdAstar.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 from pgRoutingLayer import pgRoutingLayer_utils as Utils
 from .FunctionBase import FunctionBase
+from psycopg2 import sql
 
 class Function(FunctionBase):
 
-    version = 2.0
+    minPGRversion = 2.5
 
     @classmethod
     def getName(self):
@@ -13,89 +14,41 @@ class Function(FunctionBase):
 
     @classmethod
     def getControlNames(self, version):
-        ''' returns control names. '''
-        self.version = version
-        if self.version < 2.5:
-            return self.commonControls + self.commonBoxes + self.astarControls + [
-                    'labelSourceId', 'lineEditSourceId', 'buttonSelectSourceId',
-                    'labelTargetId', 'lineEditTargetId', 'buttonSelectTargetId',
-                    ]
-        else:
-            return self.commonControls + self.commonBoxes + self.astarControls + [
-                    'labelSourceIds', 'lineEditSourceIds', 'buttonSelectSourceIds',
-                    'labelTargetIds', 'lineEditTargetIds', 'buttonSelectTargetIds',
-                    ]
+        ''' returns control names for this function. '''
+        return self.commonControls + self.commonBoxes + self.astarControls + [
+                'labelSourceIds', 'lineEditSourceIds', 'buttonSelectSourceIds',
+                'labelTargetIds', 'lineEditTargetIds', 'buttonSelectTargetIds',
+                ]
 
     def prepare(self, canvasItemList):
-        if self.version < 2.5:
-            resultPathRubberBand = canvasItemList['path']
-            resultPathRubberBand.reset(Utils.getRubberBandType(False))
-        else:
-            resultPathsRubberBands = canvasItemList['paths']
-            for path in resultPathsRubberBands:
-                path.reset(Utils.getRubberBandType(False))
-            canvasItemList['paths'] = []
+        resultPathsRubberBands = canvasItemList['paths']
+        for path in resultPathsRubberBands:
+            path.reset(Utils.getRubberBandType(False))
+        canvasItemList['paths'] = []
 
 
     def getQuery(self, args):
         ''' returns the sql query in required signature format of pgr_bdAstar '''
-        args['where_clause'] = self.whereClause(args['edge_table'], args['geometry'], args['BBOX'])
-        if self.version < 2.5:
-            return """
-                SELECT seq, id1 AS _node, id2 AS _edge, cost AS _cost
-                FROM pgr_bdAstar('
-                    SELECT %(id)s::int4 AS id,
-                        %(source)s::int4 AS source,
-                        %(target)s::int4 AS target,
-                        %(cost)s::float8 AS cost
-                        %(reverse_cost)s,
-                        %(x1)s::float8 AS x1,
-                        %(y1)s::float8 AS y1,
-                        %(x2)s::float8 AS x2,
-                        %(y2)s::float8 AS y2
-                    FROM %(edge_table)s
-                    %(where_clause)s',
-                    %(source_id)s::int4, %(target_id)s::int4, %(directed)s, %(has_reverse_cost)s)
-            """ % args
-        else:
-            return """
-                SELECT seq, '(' || start_vid || ',' || end_vid || ')' AS path_name,
-                    path_seq AS _path_seq, start_vid AS _start_vid, end_vid AS _end_vid,
-                    node AS _node, edge AS _edge, cost AS _cost, lead(agg_cost) over() AS _agg_cost
-                FROM pgr_bdAstar('
-                    SELECT %(id)s AS id,
-                        %(source)s AS source,
-                        %(target)s AS target,
-                        %(cost)s AS cost
-                        %(reverse_cost)s,
-                        %(x1)s AS x1,
-                        %(y1)s AS y1,
-                        %(x2)s AS x2,
-                        %(y2)s AS y2
-                    FROM %(edge_table)s
-                    %(where_clause)s',
-                    array[%(source_ids)s]::BIGINT[], array[%(target_ids)s]::BIGINT[], %(directed)s)
-                """ % args
+        return sql.SQL("""
+            SELECT seq, '(' || start_vid || ',' || end_vid || ')' AS path_name,
+                path_seq AS _path_seq, start_vid AS _start_vid, end_vid AS _end_vid,
+                node AS _node, edge AS _edge, cost AS _cost, lead(agg_cost) over() AS _agg_cost
+            FROM pgr_bdAstar('
+                {innerQuery}
+                ',
+                {source_ids}, {target_ids}, {directed})
+            """).format(**args)
 
     def getExportQuery(self, args):
         return self.getJoinResultWithEdgeTable(args)
 
     def getExportMergeQuery(self, args):
-        if self.version < 2.5:
-            return self.getExportOneSourceOneTargetMergeQuery(args)
-        else:
-            return self.getExportManySourceManyTargetMergeQuery(args)
+        return self.getExportManySourceManyTargetMergeQuery(args)
 
 
     def draw(self, rows, con, args, geomType, canvasItemList, mapCanvas):
-        ''' draws the result '''
-        if self.version < 2.5:
-            self.drawOnePath(rows, con, args, geomType, canvasItemList, mapCanvas)
-        else:
-            self.drawManyPaths(rows, con, args, geomType, canvasItemList, mapCanvas)
-
-
-
+        ''' draw the result. '''
+        self.drawManyPaths(rows, con, args, geomType, canvasItemList, mapCanvas)
 
     def __init__(self, ui):
         FunctionBase.__init__(self, ui)
