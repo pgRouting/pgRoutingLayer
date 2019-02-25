f8720a15b7d9d45f729dfabd34c8f842f77bedd6
Martha Vergara <mvergaratrejo@gmail.com>
2019-02-25 13:55:28 -0600

    [admin]adding licence to file pgRoutingLayer.py

--- pgRoutingLayer.py
+++ pgRoutingLayer.py
@@ -1,24 +1,32 @@
-"""
-/***************************************************************************
- pgRouting Layer
-                                 a QGIS plugin
+# -*- coding: utf-8 -*-
+# /*PGR-GNU*****************************************************************
+# File: dbConnection.py
+# 
+# Copyright (c) 2011~2019 pgRouting developers
+# Mail: project@pgrouting.org
+# 
+# Developer's GitHub nickname: 
+# - AasheeshT
+# - cayetanobv
+# - sanak 
+# - veniversum
+# - cvvergara
+# - anitagraser
+# ------
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# ********************************************************************PGR-GNU*/
 
- based on "Fast SQL Layer" plugin. Copyright 2011 Pablo Torres Carreira
-                             -------------------
-        begin                : 2011-11-25
-        copyright            : (c) 2011 by Anita Graser
-        email                : anita.graser.at@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
 from __future__ import absolute_import
 # Import the PyQt and QGIS libraries
 from qgis.PyQt import uic
